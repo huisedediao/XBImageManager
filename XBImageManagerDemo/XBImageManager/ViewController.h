5ef40c1332c7555ec973350d75ee1431157c3e28
huisedediao <401758785@qq.com>
2019-07-11 09:05:25 +0800

    base commit

---
+++ XBImageManagerDemo/XBImageManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBImageManager
+//
+//  Created by xxb on 2019/7/11.
+//  Copyright © 2019 xxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XBImageManagerDemo/XBImageManager/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XBImageManager
+//
+//  Created by xxb on 2019/7/11.
+//  Copyright © 2019 xxb. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
